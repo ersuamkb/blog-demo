b98ab16a9a3764bcf37a436a6c06d20abc1fa00e
wangqiang <ersuamkb@gmail.com>
2013-11-15 16:27:01 +0800

    version1.5

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,20 +1,12 @@
 @extends('common')
 @section('content')
 @foreach ($articles as $article)
-       {{{ $article->id}}}||
-       {{{ $article->title}}}||
-       {{{ $article->abstract}}}||
-       {{{ $article->author}}}||
-       {{{ $article->categorys_id}}}||
-       {{{ $article->commented_times}}}||
-       {{{ $article->content}}}
-
-<div class="row">
+  <div class="row">
     <div class="col-md-8">
         <!-- Post Title -->
         <div class="row">
             <div class="col-md-8">
-                <h4><strong><a href="{{{ $article->url() }}}">{{ String::title($article->title) }}</a></strong></h4>
+                <h4><strong><a href="#">{{ $article->title }}</a></strong></h4>
             </div>
         </div>
         <!-- ./ post title -->
@@ -22,13 +14,13 @@
         <!-- Post Content -->
         <div class="row">
             <div class="col-md-2">
-                <a href="{{{ $article->url() }}}" class="thumbnail"><img src="http://placehold.it/260x180" alt=""></a>
+                <a href="#" class="thumbnail"><img src="http://placehold.it/260x180" alt=""></a>
             </div>
             <div class="col-md-6">
                 <p>
-                    {{ String::tidy(Str::limit($article->content, 200)) }}
+                    {{ $article->content }}
                 </p>
-                <p><a class="btn btn-mini btn-default" href="{{{ $article->url() }}}">Read more</a></p>
+                <p><a class="btn btn-mini btn-default" href="#">Read more</a></p>
             </div>
         </div>
         <!-- ./ post content -->
@@ -38,9 +30,9 @@
             <div class="col-md-8">
                 <p></p>
                 <p>
-                    <span class="glyphicon glyphicon-user"></span> by <span class="muted">{{{ $article->author->username }}}</span>
-                    | <span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{{ $article->date() }}}
-                    | <span class="glyphicon glyphicon-comment"></span> <a href="{{{ $article->url() }}}#comments">{{$article->comments()->count()}} {{ \Illuminate\Support\Pluralizer::plural('Comment', $article->comments()->count()) }}</a>
+                    <span class="glyphicon glyphicon-user"></span> by <span class="muted">{{$article->author}}</span>
+                    | <span class="glyphicon glyphicon-calendar"></span> <!--Sept 16th, 2012-->{{$article->created_at}}
+                    | <span class="glyphicon glyphicon-comment"></span> <a href="#">{{$article->commented_times}}}</a>
                 </p>
             </div>
         </div>
@@ -49,7 +41,7 @@
 </div>
 
 <hr />
-       <br>
-       <hr>
+       
 @endforeach
+
 @stop
